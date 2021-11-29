04cf61de882eb6c3d5e5c286d52f395371fb7893
Leo.Wan <1830802211@qq.com>
2021-11-29 16:46:36 +0800

    feat(decimal/float/double/int/uint/long/ulong) add scientific notation

---
+++ Leo.Extensions.Decimal/ScientificNotationExtension.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leo.Extensions
+{
+    public static class ScientificNotationExtension
+    {
+        public static string ToScientificNotation(this int i, int precision = 2)
+        {
+            return i.ToString($"e{precision}");
+        }
+
+        public static string ToScientificNotation(this uint i, int precision = 2)
+        {
+            return i.ToString($"e{precision}");
+        }
+
+        public static string ToScientificNotation(this long l, int precision = 2)
+        {
+            return l.ToString($"e{precision}");
+        }
+
+        public static string ToScientificNotation(this ulong l, int precision = 2)
+        {
+            return l.ToString($"e{precision}");
+        }
+
+        public static string ToScientificNotation(this decimal d, int precision = 2)
+        {
+            return d.ToString($"e{precision}");
+        }
+
+        public static string ToScientificNotation(this float f, int precision = 2)
+        {
+            return f.ToString($"e{precision}");
+        }
+
+        public static string ToScientificNotation(this double d, int precision = 2)
+        {
+            return d.ToString($"e{precision}");
+        }
+    }
+}
