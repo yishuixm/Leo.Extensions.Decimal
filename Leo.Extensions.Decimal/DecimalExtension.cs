a283ace409bf096deb7249a2f9d56cceafd13f72
Leo.Wan <1830802211@qq.com>
2021-12-02 14:02:34 +0800

    fix(ValidDecimal) decimals bug when input is int or low precision decimal.

--- Leo.Extensions.Decimal/DecimalExtension.cs
+++ Leo.Extensions.Decimal/DecimalExtension.cs
@@ -19,6 +19,7 @@ namespace Leo.Extensions
             {
                 var valueStr = $"{value}";
                 string[] pieces = valueStr.Split('.');
+                if (pieces.Length == 1) pieces = new string[] { pieces[0], "0" };
                 string iPart = pieces[0], dPart = pieces[1];
 
                 if (scientificNotation)
@@ -26,25 +27,15 @@ namespace Leo.Extensions
 
                 if (!scientificNotation)
                 {
-                    if (dPart.Length > 10) dPart = dPart.Substring(0, 10);
-                    var formatDPart = int.Parse(dPart).ToString(); // 获取无前置零的小数部分
-                    var zeroCount = dPart.Length - formatDPart.Length; // 计算前置零个数
-                    if (formatDPart.Length < precision) precision = formatDPart.Length;
-                    var rDPart = formatDPart.Substring(0, precision); // 获取小数的精度
+                    int dPrecision = precision - iPart.Length;
 
-                    decimal d;
-                    if (!decimal.TryParse($"{iPart}.{rDPart.PadLeft(zeroCount + precision, '0')}", out d))
+                    if (dPrecision > 0)
                     {
-                        d = 0;
-                    }
-
-                    if (d == 0M)
-                    {
-                        ret = $"0.{"".PadLeft(precision, '0')}";
+                        ret = $"{iPart}.{dPart.PadLeft(dPrecision, '0').Substring(0, dPrecision)}";
                     }
                     else
                     {
-                        ret = $"{d}";
+                        ret = iPart;
                     }
                 }
                 else
